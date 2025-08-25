a2f979c9aa52a71da26b9061ccae2268b76b5e1a
Jabari_Lu <jabari123@jabari.tech>
2025-08-25 16:45:03 +0800

    认证服务的启动类

---
+++ auth-service/src/main/java/tech/jabari/auth/AuthApp.java
@@ -0,0 +1,19 @@
+package tech.jabari.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableFeignClients(basePackages = "tech.jabari.api.client")
+@ComponentScan(basePackages = {"tech.jabari.auth", "tech.jabari.common"})
+//@MapperScan("tech.jabari.auth.mapper")
+@SpringBootApplication
+public class AuthApp {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(AuthApp.class, args);
+        String port = ctx.getEnvironment().getProperty("server.port");
+        System.out.println("-----认证服务启动成功，端口号为："+port );
+    }
+}
